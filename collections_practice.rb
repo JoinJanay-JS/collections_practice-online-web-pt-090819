571793fc593aee6d174737ba5b81b0eafaa80b3d
Janay Bailey <tjanay87@gmail.com>
2019-09-24 13:48:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,13 +8,13 @@ def sort_array_desc(array)
   end
 end 
 
-def sort_array_char_count
+def sort_array_char_count(array)
   array.sort do |left, right|
     left.length <=> right.length
   end
 end 
 
-def swap_elements
+def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
 end
@@ -24,4 +24,14 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
-  
+  array.each do |item|
+    item[2] = "$"
+  end
+end 
+
+def find_a(array)
+  array.find_all do |word|
+    word[0] == "a"
+  end
+end
+
